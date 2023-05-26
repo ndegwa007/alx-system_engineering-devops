bd3d4639d29e9ca76d216bec6ec1027ec824842d
ndegz <ndegwa8781@gmail.com>
2023-05-26 17:03:46 +0300

    gather data from an api

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""a script that shows employees progress completing tasks
-    from all the allocated tasks"""
+"""a script that shows employees all the allocated tasks and writes it to a csv file"""
 import requests
 import sys
+import csv
 
-
-def get_employee_progress(employee_id):
+def get_employee_progress_to_csv(employee_id):
     """ get the api url and find employee progress """
 
     base_url = "https://jsonplaceholder.typicode.com"
@@ -16,7 +15,8 @@ def get_employee_progress(employee_id):
     resp = requests.get(employee_url)
     resp.raise_for_status()
     employee_data = resp.json()
-
+    print(employee_data)
+    return
     # retrieve employee todo list
     resp = requests.get(todos_url)
     resp.raise_for_status()
@@ -29,14 +29,21 @@ def get_employee_progress(employee_id):
             todo['title'] for todo in todos_data if todo['completed']]
 
     # display progress info
-    print(f"Employee {employee_name} is done with \
-tasks({len(completed_tasks)}/{todo_task}):")
 
-    for task in completed_tasks:
-        print(f"\t {task}")
+    """csvheader = ["userId", "username", "completed","title"]
+    output = []
+
+    for i in employee_data:
+        listing = [i['userId'], i['username'], i['completed'], i['title']]
+        output.append(listing]
+
+    with open(f"{employers_id}.csv", "w", "utf-8", newline="") as f:
+        writer = csv.writer(f)
+        
+        csv.writerow(csvheader)
+        csv.writerows(output)"""
 
 
-if __name__ == '__main__':
-    """ get the employee progress """
-    employee_id = sys.argv[1]
-    get_employee_progress(employee_id)
+#if __name__ == '__main__':
+    """ write the employee progress to a csv file """
+    
