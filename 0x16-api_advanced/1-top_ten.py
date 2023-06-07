2928910b0dcbbb48bc535f6632ae2c7a59153804
ndegz <ndegwa8781@gmail.com>
2023-06-07 11:50:52 +0300

    hot topics

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ titles of the first 10 hot posts for a subreddit """
+    headers = {'User-Agent': 'super-bot/1.1'}
+    count = 9
+    response = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/hot.json?limit={count}",
+            headers=headers)
+    if response.status_code == 200:
+        response_json = response.json()
+        posts = response_json['data']['children']
+        p = [post['data']['title'] for post in posts]
+        for title in p:
+            print(title)
+    else:
+        return 0
--- 0x16-api_advanced/README.md
+++ 0x16-api_advanced/README.md
@@ -8,4 +8,7 @@ Practice using the [Reddit API](https://intranet.alxswe.com/rltoken/b-4nD6hwEeNY
 <a href="https://postimg.cc/jw54Nq7P" target='_blank'><img src='https://i.postimg.cc/NM8J35GJ/Screenshot-from-2023-06-07-08-50-35.png' border='0' alt='image'/></a>
 </details>
 
-
+<details>
+<summary><a href="./1-top_ten.py">1. Top ten</a></summary>
+<a href="https://postimg.cc/G8dBhp1F" target='_blank'><img src='https://i.postimg.cc/gJ63swST/Screenshot-from-2023-06-07-10-37-17.png' border='0' alt='image'/></a>
+</details>
