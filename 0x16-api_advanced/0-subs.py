37db36f9b282b157623c6c77509b33cc0b79b26c
ndegz <ndegwa8781@gmail.com>
2023-06-07 09:56:05 +0300

    number of subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" function queries the reddit API"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ returns number of subs from a given reddit """
+
+    headers = {'User-Agent': 'super-bot/1.0'}
+    response = requests.get(
+            f"https://www.reddit.com/r/{subreddit}/about.json",
+            headers=headers)
+
+    if response.status_code == 200:
+        response_json = response.json()
+        if 'subscribers' in response_json['data']:
+            return response_json['data']['subscribers']
+    return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,11 @@
+# 0x16. API advanced
+
+Practice using the [Reddit API](https://intranet.alxswe.com/rltoken/b-4nD6hwEeNYTwYl5yWNwA)
+
+## Tasks
+<details>
+<summary><a href="./0-subs.py">0. How many subs?</a></summary>
+<a href="https://postimg.cc/jw54Nq7P" target='_blank'><img src='https://i.postimg.cc/NM8J35GJ/Screenshot-from-2023-06-07-08-50-35.png' border='0' alt='image'/></a>
+</details>
+
+
