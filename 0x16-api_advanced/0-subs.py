34d1ce81e25bd67cd58a89187fc14965f35f847f
ndegz <ndegwa8781@gmail.com>
2023-06-07 10:14:49 +0300

    number of subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """queries the reddit API"""
+
 import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-
+    """ function gets number of subs in a subreddit """
     headers = {'User-Agent': 'super-bot/1.0'}
     response = requests.get(
             f"https://www.reddit.com/r/{subreddit}/about.json",
