f6ed760a8121de675ace09f1df1d54379c763b60
ndegz <ndegwa8781@gmail.com>
2023-06-19 09:09:17 +0300

    check nginx server under pressure

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,14 +4,13 @@
 import requests
 
 
-def recurse(subreddit):
+def recurse(subreddit, hot_post=[]):
     """ query all hot topics from subreddit """
     headers = {'User-Agent': 'super-bot/1.2'}
     base_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    response = requests.get(base_url)
+    response = requests.get(base_url, headers=headers)
 
     if response.status_code == 200:
         response_json = response.json()
         posts = response_json['data']['children']
-        
-
+                
