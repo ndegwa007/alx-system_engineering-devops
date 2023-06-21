523bed20ffc7170fd2277b5d24fee1a192b46637
ndegz <ndegwa8781@gmail.com>
2023-06-21 10:26:48 +0300

    recurse call on redditAPI

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,13 +4,33 @@
 import requests
 
 
-def recurse(subreddit, hot_post=[]):
+def recurse(subreddit, after=None, hot_topics=[]):
     """ query all hot topics from subreddit """
-    headers = {'User-Agent': 'super-bot/1.2'}
-    base_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    response = requests.get(base_url, headers=headers)
 
-    if response.status_code == 200:
-        response_json = response.json()
-        posts = response_json['data']['children']
-                
+    # base case
+    if hot_topics is None:
+        return []
+
+    base_url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'super-bot/1.2'}
+    params = {'limit': 100}
+
+    if after:
+        params['after'] = after
+    try:
+        response = requests.get(base_url, params=params, headers=headers)
+    except Exception as e:
+        print(f"An error occurred during the API request: {e}")
+
+    # get json data
+    try:
+        data = response.json()['data']
+    except Exception as e:
+        print(f"KeyError: {e}")
+
+    for child in data['children']:
+        hot_topics.append(child['data']['title'])
+
+    if data['after'] is not None:
+        recurse(subreddit, data['after'], hot_topics)
+    return hot_topics
